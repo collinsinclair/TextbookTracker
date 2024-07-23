0a2f36266982817c4889481d8e4020deee27c651
Collin Sinclair <collin@collinsinclair.com>
2024-07-23 11:50:59 -0600

    add models

--- content/models.py
+++ content/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    edition = models.IntegerField()
+    year = models.IntegerField()
+
+
+class Chapter(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        authors = ", ".join([author.last_name for author in self.book.authors.all()])
+        return f"{authors} - {self.book.title}: {self.number} {self.title}"
+
+
+class Section(models.Model):
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    number = models.IntegerField()
+
+    class Meta:
+        unique_together = ["chapter", "number"]
+
+    def __str__(self):
+        return f"{self.chapter}.{self.number} {self.title}"
+
+
+class Problem(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    number = models.IntegerField()
+
+    class Meta:
+        unique_together = ["section", "number"]
+
+    def __str__(self):
+        authors = ", ".join(
+            [author.last_name for author in self.section.chapter.book.authors.all()]
+        )
+        return f"{authors} {self.section.chapter.number}.{self.number} ({self.section.title})"
