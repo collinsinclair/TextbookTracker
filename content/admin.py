73fbc3e771b35fbfc361f5401cbefade2f16fbdd
Collin Sinclair <collin@collinsinclair.com>
2024-07-23 12:03:30 -0600

    register models in admin

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+models = [Author, Book, Chapter, Section, Problem]
+
+for model in models:
+    admin.site.register(model)
